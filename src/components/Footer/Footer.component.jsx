0628ecd1bbc44413fa1414a41e59480472589b35
Pratyush Bhatnagar <38534434+pratyushbh@users.noreply.github.com>
2022-11-11 15:54:34 +0530

    finallized

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { CDBFooter, CDBFooterLink, CDBBox, CDBBtn, CDBIcon } from 'cdbreact';
+import "./Footer.styles.scss";
+import om from '../../assets/ompinkgrad.png'
 
 const Footer = () => {
   const bgBlack = { backgroundColor: '#ffa1a1', color: 'white' };
@@ -8,58 +10,37 @@ const Footer = () => {
     <CDBFooter style={bgBlack} className="shadow" >
       <CDBBox display="flex" flex="column" className="mx-auto py-5" style={{ width: '90%' }}>
         <CDBBox display="flex" justifyContent="between" className="flex-wrap">
+          <CDBBox> 
+                <div className='Hero-title'>
+                    <p className='hero_p'>SAI<img className='imgg' src={om} alt="om"/>YO</p>
+                </div>
+          </CDBBox>
           <CDBBox>
-            <a href="/" className="d-flex align-items-center p-0 text-dark">  
-              <span className="ml-3 h5 font-weight-bold">Saiyomyo</span>
-            </a>
-            <p className="my-3" style={{ width: '250px' }}>
-              
+            <p className="h5 mb-4" style={{ fontWeight: '600' }}>
+              Home
             </p>
-            <CDBBox display="flex" className="mt-4">
-              <CDBBtn flat color="dark">
-                <CDBIcon fab icon="facebook-f" />
-              </CDBBtn>
-              <CDBBtn flat color="dark" className="mx-3">
-                <CDBIcon fab icon="twitter" />
-              </CDBBtn>
-              <CDBBtn flat color="dark" className="p-2">
-                <CDBIcon fab icon="instagram" />
-              </CDBBtn>
+            <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
+              <CDBFooterLink href="/">Homepage</CDBFooterLink>
             </CDBBox>
           </CDBBox>
           <CDBBox>
             <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-              Devwares
+              Social Media
             </p>
             <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-              <CDBFooterLink href="/">Resources</CDBFooterLink>
-              <CDBFooterLink href="/">About Us</CDBFooterLink>
-              <CDBFooterLink href="/">Contact</CDBFooterLink>
-              <CDBFooterLink href="/">Blog</CDBFooterLink>
+              <CDBFooterLink href="/">Instagram</CDBFooterLink>
             </CDBBox>
           </CDBBox>
           <CDBBox>
             <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-              Help
+              Canclation Policies
             </p>
             <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-              <CDBFooterLink href="/">Support</CDBFooterLink>
-              <CDBFooterLink href="/">Sign Up</CDBFooterLink>
-              <CDBFooterLink href="/">Sign In</CDBFooterLink>
-            </CDBBox>
-          </CDBBox>
-          <CDBBox>
-            <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-              Products
-            </p>
-            <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-              <CDBFooterLink href="/">Windframe</CDBFooterLink>
-              <CDBFooterLink href="/">Loop</CDBFooterLink>
-              <CDBFooterLink href="/">Contrast</CDBFooterLink>
+              <CDBFooterLink href="/">Documents</CDBFooterLink>
             </CDBBox>
           </CDBBox>
         </CDBBox>
-        <small className="text-center mt-5">&copy; Devwares, 2020. All rights reserved.</small>
+        <small className="text-center mt-5">&copy; Saiomyo, 2022. All rights reserved.</small>
       </CDBBox>
     </CDBFooter>
   );
