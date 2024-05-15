2f114355f04d50c8c4e5a6629367240987330c0e
Mridulish_26 <91665131+mridul-mathur@users.noreply.github.com>
2024-05-16 01:10:28 +0530

    project push

--- src/components/Button/Button.component.jsx
+++ src/components/Button/Button.component.jsx
@@ -5,13 +5,12 @@ class Button extends Component {
     render() {
         const {color,background,Link}=this.props;
         return (
-            <div className='Buttonn'>
-                <button style={{marginLeft:'265%',width:'135%',border:0,paddingLeft:'15px',paddingRight:'15px',background:background,backgroundColor:color}}>
+            <div className='Button'>
+                <button style={{marginLeft:'265%',width:'135%',border:0,paddingLeft:'15px',paddingRight:'15px',background:background,backgroundColor:color,borderRadius:'0.5rem'}}>
                     {this.props.children}
                 </button>
             </div>
         );
     }
 }
-
 export default Button;
